df0204df4b847543144361f7d166ff7d687ecd8b
Junko Werner <aliciajace@gmail.com>
2015-08-24 16:56:10 -0700

    incomplete User Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   resources :lists do
     resources :items 
   end
+
+   namespace :api, defaults: { format: :json } do
+     resources :users
+   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
